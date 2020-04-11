6e601baa32707f67e635d66c90b873046e8a42c1
Niklas Heim <heim.niklas@gmail.com>
2020-04-11 12:18:52 +0200

    l1/l2 first

--- scripts/division/run_all.sh
+++ scripts/division/run_all.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
+export JULIA_NUM_THREADS=16
 echo JULIA_NUM_THREADS: $JULIA_NUM_THREADS
 
-julia --project ard_xovery.jl
-julia --project msel1_xovery.jl
-julia --project msel2_xovery.jl
+julia --project --color=yes msel1_xovery.jl
+julia --project --color=yes msel2_xovery.jl
+julia --project --color=yes ard_xovery.jl
