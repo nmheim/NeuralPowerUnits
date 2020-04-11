ccf271ae1a3000de2df82b0f8bc79741526dc9a5
Niklas Heim <heim.niklas@gmail.com>
2020-04-11 11:48:43 +0200

    fix l2

--- scripts/division/msel2_xovery.jl
+++ scripts/division/msel2_xovery.jl
@@ -8,6 +8,7 @@ using Distributions: Uniform
 using Parameters
 using ValueHistories
 using LinearAlgebra
+using GMExtensions
 
 include(joinpath(@__DIR__, "utils.jl"))
 include(srcdir("utils.jl"))
@@ -20,51 +21,17 @@ include(srcdir("utils.jl"))
     lr::Real        = 0.001
     lowlim::Int     = 1
     uplim::Int      = 3
-    initnau::String = "diag"
-    initnmu::String = "zero"
-end
-
-function init_diag(T, a::Int, b::Int)
-    m = zeros(T,a,b)
-    m[diagind(m,0)] .= 1f0
-    return m
-end
-
-function initf(s::String)
-    if s == "diag"
-        return (a,b) -> init_diag(Float32,a,b)
-    elseif s == "glorotuniform"
-        return (a,b) -> Flux.glorot_uniform(a,b)
-    elseif s == "rand"
-        return (a,b) -> rand(Float32,a,b)
-    elseif s == "randn"
-        return (a,b) -> randn(Float32,a,b)
-    elseif s == "zero"
-        return (a,b) -> zeros(Float32,a,b)
-    else
-        throw(ArgumentError("Unkown init: $init"))
-    end
-end
-
-function xovery(x::Array{T,2}) where T
-    x1 = x[1,:]
-    x2 = x[2,:]
-    y = x1 ./ x2
-    reshape(y, 1, :)
-end
-
-function generate(inlen::Int, batch::Int, r::Uniform)
-    x = Float32.(rand(r, inlen, batch))
-    y = xovery(x)
-    (x,y)
+    βL2             = 10
+    initnau::String = "rand"
+    initnmu::String = "rand"
 end
 
 function run(config)
-    @unpack initnau, initnmu, lowlim, uplim = config
+    @unpack initnau, initnmu, lowlim, uplim, βL2 = config
     @unpack niters, batch, inlen, outlen, lr = config
     model   = mapping(inlen, outlen, initf(initnau), initf(initnmu))
     ps      = params(model)
-    loss    = (x,y) -> Flux.mse(x,y) + norm(ps, 2)
+    loss    = (x,y) -> Flux.mse(model(x),y) + βL2*norm(ps, 2)
     range   = Uniform(lowlim,uplim)
     data    = (generate(inlen,batch,range) for _ in 1:niters)
     opt     = RMSProp(lr)
@@ -74,29 +41,30 @@ end
 
 #################### Single run with default params ############################
 
+# pattern = "division_msel2_xovery_run1"
 # config = Config()
-# res, fname = produce_or_load(datadir("division_xovery"), config, run)
+# res, fname = produce_or_load(datadir(pattern), config, run, force=true)
 # 
 # m = res[:model]
 # h = res[:history]
 # 
 # pyplot()
 # p1 = plothistory(h)
-# net = get_mapping(m)
 # p2 = plot(
-#     annotatedheatmap(net[1].W[end:-1:1,:], c=:bluesreds, title="NAU", clim=(-1,1)),
-#     annotatedheatmap(net[2].W[end:-1:1,:], c=:bluesreds, title="ReNMUX", clim=(-1,1)),
+#     annotatedheatmap(m[1].W[end:-1:1,:], c=:bluesreds, title="NAU", clim=(-1,1)),
+#     annotatedheatmap(m[2].W[end:-1:1,:], c=:bluesreds, title="ReNMUX", clim=(-1,1)),
 #     size=(600,300))
-# wsave(plotsdir("division_xovery", "$(basename(splitext(fname)[1]))-history.svg"), p1)
-# wsave(plotsdir("division_xovery", "$(basename(splitext(fname)[1]))-mapping.svg"), p2)
-
+# wsave(plotsdir(pattern, "$(basename(splitext(fname)[1]))-history.svg"), p1)
+# wsave(plotsdir(pattern, "$(basename(splitext(fname)[1]))-mapping.svg"), p2)
+# error()
 
 ################################################################################
 
 
 # set up dict which will be permuted to yield all config combinations
 config_dicts = Dict(
-    :init => [("diag", "zero"), ("rand","rand"),
+    :βL2 => 10f0 .^ (-1f0:2f0),
+    :init => [("diag", "zero"), ("diag","one"), ("rand","rand"),
               ("glorotuniform", "glorotuniform"),
               ("randn","randn")])
 
--- scripts/division/utils.jl
+++ scripts/division/utils.jl
@@ -1,3 +1,41 @@
+function init_diag(T, a::Int, b::Int)
+    m = zeros(T,a,b)
+    m[diagind(m,0)] .= 1f0
+    return m
+end
+
+function initf(s::String)
+    if s == "diag"
+        return (a,b) -> init_diag(Float32,a,b)
+    elseif s == "glorotuniform"
+        return (a,b) -> Flux.glorot_uniform(a,b)
+    elseif s == "rand"
+        return (a,b) -> rand(Float32,a,b)
+    elseif s == "randn"
+        return (a,b) -> randn(Float32,a,b)
+    elseif s == "zero"
+        return (a,b) -> zeros(Float32,a,b)
+    elseif s== "one"
+        return (a,b) -> ones(Float32,a,b)
+    else
+        throw(ArgumentError("Unkown init: $s"))
+    end
+end
+
+function xovery(x::Array{T,2}) where T
+    x1 = x[1,:]
+    x2 = x[2,:]
+    y = x1 ./ x2
+    reshape(y, 1, :)
+end
+
+function generate(inlen::Int, batch::Int, r::Uniform)
+    x = Float32.(rand(r, inlen, batch))
+    y = xovery(x)
+    (x,y)
+end
+
+
 function mapping(in, out, init_nau, init_nmu)
     nau = NAU(in, in, init=init_nau)
     nmu = ReNMUX(in, out, init=init_nmu)
@@ -8,12 +46,12 @@ function train!(loss, model, data, opt, history=MVHistory())
     ps = params(model)
     train_loss = 0f0
 
-    logging = Flux.throttle((i)->(@info "Step $i" [tot llh kld lpλ]), 1)
+    logging = Flux.throttle((i)->(@info "Step $i: $train_loss"), 1)
     pushhist = Flux.throttle((i)->(
-            push!(history, :μz, i, Flux.destructure(model)[1]);
+            push!(history, :μz, i, copy(Flux.destructure(model)[1]));
             push!(history, :loss, i, [train_loss]);
        ),
-    0.5)
+    0.1)
 
     i = haskey(history,:loss) ? get(history,:loss)[1][end]+1 : 1
     try 
@@ -23,7 +61,7 @@ function train!(loss, model, data, opt, history=MVHistory())
                 return train_loss
             end
             # logging(i)
-            #pushhist(i)
+            pushhist(i)
             Flux.Optimise.update!(opt, ps, gs)
             i += 1
         end
