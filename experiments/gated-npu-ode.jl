113bd012f4e342258385ad3eb04c6cdce2ee75f4
Niklas Heim <heim.niklas@gmail.com>
2020-05-18 17:48:07 +0200

    ANCHOR: learning lotka volterra

--- experiments/gated-npu-ode.jl
+++ experiments/gated-npu-ode.jl
@@ -1,6 +1,7 @@
 using DiffEqFlux
 using OrdinaryDiffEq
 using Flux
+using LinearAlgebra
 using NeuralArithmetic
 using Plots
 unicodeplots()
@@ -11,27 +12,27 @@ tspan = (0.0,1.5)
 
 function trueODEfunc(du,u,p,t)
     true_A = [-0.1 2.0; -2.0 -0.1]
-    du .= ((u.^3)'true_A)'
+    du .= ((u.^1)'true_A)'
 end
 t = range(tspan[1],tspan[2],length=datasize)
 prob = ODEProblem(trueODEfunc,u0,tspan)
 ode_data = Array(solve(prob,Tsit5(),saveat=t))
 
-# function lotka_volterra(du,u,p,t)
-#   x, y = u
-#   α, β, δ, γ = p
-#   du[1] = dx = α*x - β*x*y
-#   du[2] = dy = -δ*y + γ*x*y
-# end
-# u0 = [1.0,1.0]
-# tspan = (0.0,7.0)
-# truep = [1.1,1.0,1.3,1.0]
-# t = range(tspan[1],tspan[2],length=datasize)
-# prob = ODEProblem(lotka_volterra,u0,tspan,truep)
-# ode_data = Array(solve(prob, Tsit5(), saveat=t))
+function lotka_volterra(du,u,p,t)
+  x, y = u
+  α, β, δ, γ = p
+  du[1] = dx = α*x - β*x*y
+  du[2] = dy = -δ*y + γ*x*y
+end
+u0 = [0.5,1.0]
+tspan = (0.0,7.0)
+truep = [1.1,1.0,1.3,1.0]
+t = range(tspan[1],tspan[2],length=datasize)
+prob = ODEProblem(lotka_volterra,u0,tspan,truep)
+ode_data = Array(solve(prob, Tsit5(), saveat=t))
 
 
-init(a,b) = rand(Float64,a,b)/10
+init(a,b) = rand(Float64,a,b)/2
 init(a,b) = Float64.(Flux.glorot_uniform(a,b))
 # dudt = Chain(NAU(2,10),
 #              GatedNPU(10,10,init=init),
@@ -40,15 +41,24 @@ init(a,b) = Float64.(Flux.glorot_uniform(a,b))
 dudt = Chain(GatedNPU(2,10,init=init),
              NAU(10,2,init=init))
 
-dudt = Chain(GatedNPUX(2,10,initRe=init, initIm=zeros),
-             NAU(10,2,init=init))
+hdim = 20
+dudt = Chain(GatedNPUX(2,hdim,initRe=init, initIm=zeros),
+             NAU(hdim,2,init=init))
+
+#dudt = Chain(NALU(2,hdim),NALU(hdim,2))
 
 # dudt = Chain(GatedNPUX([3.1 0.0; 0.0 3.1],
 #                        zeros(2,2), ones(2)),
 #              NAU([-0.1 2.0; -2.0 -0.1]))
+# dudt = Chain(NPUX([3.0 0.0; 0.0 3.0],
+#                   zeros(2,2)),
+#              NAU([-0.1 2.0; -2.0 -0.1]'))
+
+
+
 # dudt = FastChain(FastDense(2,50,tanh),
 #                  FastDense(50,2))
-n_ode = NeuralODE(dudt,tspan,Tsit5(),saveat=t)
+n_ode = NeuralODE(dudt,tspan,Euler(),saveat=t, dt=0.01)
 
 function predict_n_ode(p)
   n_ode(u0,p)
@@ -56,7 +66,7 @@ end
 
 function loss_n_ode(p)
     pred = predict_n_ode(p)
-    loss = sum(abs2,ode_data .- pred)
+    loss = sum(abs2,ode_data .- pred) + 1e-5*norm(p,1) + 0.1norm(p[hdim*2+1:hdim*4],1)
     loss,pred
 end
 
@@ -65,8 +75,10 @@ cb = function (p,l,pred;doplot=true) #callback function to observe training
   # plot current prediction against data
   @info l p'
   if doplot
-    pl = plot(t,ode_data[1,:],label="data")
+    pl = scatter(t,ode_data[1,:],label="data")
+    scatter!(pl,t,ode_data[2,:],label="data")
     plot!(pl,t,pred[1,:],label="prediction")
+    plot!(pl,t,pred[2,:],label="prediction")
     display(plot(pl))
     println("\n")
   end
@@ -76,7 +88,7 @@ end
 # Display the ODE with the initial parameter values.
 cb(n_ode.p,loss_n_ode(n_ode.p)...)
 
-res1 = DiffEqFlux.sciml_train(loss_n_ode, n_ode.p, ADAM(0.01), cb = cb, maxiters = 100)
+res1 = DiffEqFlux.sciml_train(loss_n_ode, n_ode.p, RMSProp(0.005), cb = cb, maxiters = 300)
 cb(res1.minimizer,loss_n_ode(res1.minimizer)...;doplot=true)
 @info "opt 1 done ---------------------------------------------------------"
 @info "opt 1 done ---------------------------------------------------------"
@@ -84,7 +96,7 @@ cb(res1.minimizer,loss_n_ode(res1.minimizer)...;doplot=true)
 @info "opt 1 done ---------------------------------------------------------"
 @info "opt 1 done ---------------------------------------------------------"
 
-res2 = DiffEqFlux.sciml_train(loss_n_ode, res1.minimizer, ADAM(0.001), cb = cb, maxiters = 300)
+res2 = DiffEqFlux.sciml_train(loss_n_ode, res1.minimizer, RMSProp(0.0001), cb = cb, maxiters = 3000)
 cb(res2.minimizer,loss_n_ode(res2.minimizer)...;doplot=true)
 error()
 
