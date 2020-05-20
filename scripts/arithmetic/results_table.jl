de2fda5ed8352c805fb193acf0bfe5b8a96d2e1f
Niklas Heim <heim.niklas@gmail.com>
2020-05-20 21:12:16 +0200

    plot

--- scripts/arithmetic/results_table.jl
+++ scripts/arithmetic/results_table.jl
@@ -49,24 +49,22 @@ end
 
 Base.last(h::MVHistory, k::Symbol) = get(h,k)[2][end]
 
-
-function collect_all_results!(folders)
-    df = map(folders) do folder
-        _df = collect_results!(datadir(folder), white_list=[],
-                              special_list=[:trn => data -> last(data[:history], :loss)[1],
-                                            :mse => data -> last(data[:history], :loss)[2],
-                                            :reg => data -> last(data[:history], :loss)[3],
-                                            :val => data -> last(data[:history], :loss)[4],
-                                            :config => data -> data[:c],
-                                            :hash => data -> hash(delete!(struct2dict(data[:c]),:run)),
-                                            :task => data -> split(basename(folder),"_")[1],
-                                           ],
-                             )
-        expand_config!(_df)
-        return _df
-    end
-    vcat(df...)
+function collect_folder!(folder::String)
+     _df = collect_results!(datadir(folder), white_list=[],
+                          special_list=[:trn => data -> last(data[:history], :loss)[1],
+                                        :mse => data -> last(data[:history], :loss)[2],
+                                        :reg => data -> last(data[:history], :loss)[3],
+                                        :val => data -> last(data[:history], :loss)[4],
+                                        :config => data -> data[:c],
+                                        :hash => data -> hash(delete!(struct2dict(data[:c]),:run)),
+                                        :task => data -> split(basename(folder),"_")[1],
+                                       ],
+                         )
+    expand_config!(_df)
+    return _df
 end
+
+collect_all_results!(folders::Vector{String}) = vcat(map(collect_folder!, folders)...)
 
 function best_models_for_tasks(df::DataFrame, key::String)
     best = combine(groupby(df,"model")) do modeldf
@@ -81,6 +79,7 @@ function best_models_for_tasks(df::DataFrame, key::String)
     models = unique(best, "model").model
 
     result = DataFrame(Union{Float64,Missing}, length(tasks), length(models)+1)
+    #DataFrame([Vector{t}(undef, nrows) for i = 1:ncols])
     rename!(result, vcat(["task"], models))
     result[!,1] = tasks
 
@@ -91,7 +90,6 @@ function best_models_for_tasks(df::DataFrame, key::String)
             mtdf = filter(:task=>task->task==t, mdf)
             if size(mtdf) == (1,3)
                 v = mtdf[1,key]
-                @info m,t,v
                 result[i,m] = v
             elseif size(mtdf) == (0,3)
                 result[i,m] = missing
@@ -103,19 +101,48 @@ function best_models_for_tasks(df::DataFrame, key::String)
     return result
 end
 
-key = "trn"
+function plot_result_folder(df::DataFrame, cols::Vector{String}, measure::String)
+    ps = []
+    gdf = groupby(df, "model")
+
+    modelplot = plot(title="models", yscale=:log10, legend=false)
+
+    for (name,_df) in zip(keys(gdf), gdf)
+        name = name[1]
+        m = _df[!,measure] 
+        plot!(modelplot, [name for _ in 1:length(m)], m)
+        plot!(modelplot, [name], [minimum(m)], marker="o")
+        for col in cols
+            v = _df[!,col]
+            xscale = col=="βend" ? :log10 : :identity
+            p = scatter(v,m, xlabel=col, ylabel=measure, title=name,
+                        yscale=:log10, xscale=xscale)
+            push!(ps, p)
+        end
+
+    end
+    #plot(ps...,modelplot)
+    plot(ps...,modelplot,layout=(:,3))
+end
+
+key = "val"
 folders = ["addition_npu_l1_search"
           ,"mult_npu_l1_search"
           ,"sqrt_npu_l1_search"
           ,"div_npu_l1_search"]
 folders = map(datadir, folders)
 
+# _df = collect_folder!(folders[1])
+# pyplot()
+# display(plot_result_folder(_df,["βend", "fstinit", "sndinit"], key))
+# error()
+
 df = collect_all_results!(folders)
 adf = aggregateruns(df)
 
 models = ["gatednpux","nmu","nalu"]
 bestdf = best_models_for_tasks(df, key)
-#bestdf = best_models_for_tasks(adf, "μ$key")
+bestdf = best_models_for_tasks(adf, "μ$key")
 #sort!(adf,"μ$key")
 
 # folder = folders[3]
