31fd397dd59a1ec2b2a4bdb223c2bb5a3f7d70e1
Niklas Heim <heim.niklas@gmail.com>
2020-05-26 08:29:13 +0200

    fuzz fuzzz

--- scripts/arithmetic/pareto.jl
+++ scripts/arithmetic/pareto.jl
@@ -11,6 +11,7 @@ using NeuralArithmetic
 using ValueHistories
 using DataFrames
 
+SOBOL = true
 #include(joinpath(@__DIR__, "configs.jl"))
 include(joinpath(@__DIR__, "sobolconfigs.jl"))
 include(srcdir("arithmetic_dataset.jl"))
@@ -34,7 +35,7 @@ validation_samples(c::Union{MultL1SearchConfig,AddL1SearchConfig,DivL1SearchConf
     validation_samples(c,[-4.5f0,-2.5f0,-1.5f0,-0.3f0,-0.2f0,0.1f0,1f0,2f0,3f0,10f0])
 
 validation_samples(c::SqrtL1SearchConfig) =
-    validation_samples(c,[4.5f0,2.5f0,1.5f0,0.3f0,0.2f0,0.1f0,1f0,2f0,3f0,10f0,20f0])
+    validation_samples(c,[4.5f0,2.5f0,1.5f0,0.3f0,0.2f0,0.01f0,0.1f0,1f0,2f0,3f0,10f0,20f0])
 
 function sobol_samples(c)
     s = SobolSeq(c.inlen)
@@ -108,17 +109,18 @@ nrparams(m::NAC, thresh) = sum(map(x->nrparams(x,thresh), [m.W,m.M]))
 nrparams(m::NALU, thresh) = sum(map(x->nrparams(x,thresh), [m.nac,m.G,m.b]))
 nrparams(m::Chain, thres) = sum(map(x->nrparams(x,thres), m))
 
-folders = ["addition_npu_l1_search"
+uni_folders = ["addition_npu_l1_search"
           ,"mult_npu_l1_search"
           ,"sqrt_npu_l1_search"
           ,"div_npu_l1_search"]
 
-folders = ["add_l1_runs"
+sobol_folder = ["add_l1_runs"
           ,"mult_l1_runs"
           ,"invx_l1_runs"
           ,"sqrt_l1_runs"]
+folders = SOBOL ? sobol_folder : uni_folders
 
-df = collect_all_results!(folders)
+# df = collect_all_results!(folders)
 
 task(x::Array,c::SqrtL1SearchConfig) = sqrt(x,c.subset)
 task(x::Array,c::DivL1SearchConfig) = invx(x,c.subset)
@@ -128,7 +130,7 @@ task(x::Array,c::MultL1SearchConfig) = mult(x,c.subset,c.overlap)
 function pareto(d::Dict)
     @unpack thresh = d
     c = first(filter(r->r.task=="sqrt",df)).config
-    x = validation_samples(c)
+    #x = validation_samples(c)
     x = sobol_samples(c)
     @progress for row in eachrow(df)
         m = load(row.path)[:model]
@@ -140,20 +142,27 @@ function pareto(d::Dict)
     return @dict(df)
 end
 
-(res,_) = produce_or_load(datadir("pareto"),
-                          prefix="sobol",
-                          Dict(:thresh=>1e-3),
+(res,fname) = produce_or_load(datadir("pareto"),
+                          prefix=SOBOL ? "sobol" : "",
+                          Dict(:thresh=>1e-5),
                           pareto,
                           digits=10,
                           force=false)
+display(fname)
 df = res[:df]
 
+using Plots
+pyplot()
 ps = []
 for dft in groupby(df, "task")
     s1 = plot(title=dft.task[1])
     for dfm in groupby(dft,"model")
-        scatter!(s1, log10.(dfm.val), log10.(dfm.reg),
-                 ms=5, label=dfm.model[1], alpha=0.5)
+        #if dfm.model[1] != "gatednpu"
+            scatter!(s1, log10.(dfm.reg), log10.(dfm.mse),
+            #scatter!(s1, dfm.val, dfm.reg,
+                     ylabel="log(mse)", xlabel="log(nr params)",
+                     ms=5, label=dfm.model[1], alpha=0.5, ylim=-(-2,10))
+        #end
     end
     push!(ps, s1)
 end
