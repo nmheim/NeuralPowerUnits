7879a113675f47faff05db052b525cea6f260a4c
Niklas Heim <heim.niklas@gmail.com>
2020-06-02 20:39:49 +0200

    clean up

--- scripts/old/sqrt/run_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-export JULIA_NUM_THREADS=16
-echo JULIA_NUM_THREADS: $JULIA_NUM_THREADS
-
-julia --project --color=yes msel1_sqrt.jl
-julia --project --color=yes msel2_sqrt.jl
-julia --project --color=yes ard_sqrt.jl
--- src/createlhc.jl
+++
@@ -1,5 +0,0 @@
-using LatinHypercudeSampling
-
-function lhs(xlen::Int, batch::Int)
-    LHCoptim
-end
